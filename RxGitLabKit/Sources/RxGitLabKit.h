caa375b127ef4d27d1b7dc3c0dea31e712f7ce10
Dagy Tran <dagytran@gmail.com>
2018-08-03 10:38:34 +0200

    Cross-Platform Framework setup

---
+++ RxGitLabKit/Sources/RxGitLabKit.h
@@ -0,0 +1,18 @@
+//
+//  RxGitLabKit.h
+//  RxGitLabKit
+//
+//  Created by Dagy Tran on 03/08/2018.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for RxGitLabKit_iOS.
+FOUNDATION_EXPORT double RxGitLabKit_VersionNumber;
+
+//! Project version string for RxGitLabKit_iOS.
+FOUNDATION_EXPORT const unsigned char RxGitLabKit_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RxGitLabKit/PublicHeader.h>
+
+
