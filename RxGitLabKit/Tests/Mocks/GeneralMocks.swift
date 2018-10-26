957187a78b451fcb0f7c18a082a5674f033d061d
Dagy Tran <dagytran@gmail.com>
2018-10-26 15:22:36 +0200

    Project Fix

--- RxGitLabKit/Tests/Mocks/GeneralMocks.swift
+++ RxGitLabKit/Tests/Mocks/GeneralMocks.swift
@@ -12,9 +12,15 @@ class GeneralMocks {
 {"error":"invalid_request","error_description":"The request is missing a required parameter, includes an unsupported parameter value, or is otherwise malformed."}
 """.data()
   
-  static let mockLogin = ["username" : "tranaduc",
-                          "password" : "nV4-ubr-M8V-LFx",
-                          "oAuthToken" : "e379c3dd992dfb8043db912bb8ad6643130848184edad33358029a3176cabaec"
+  enum LoginFields {
+    case username
+    case password
+    case oAuthToken
+  }
+  
+  static let mockLogin = [LoginFields.username : "tranaduc",
+                          LoginFields.password : "nV4-ubr-M8V-LFx",
+                          LoginFields.oAuthToken : "e379c3dd992dfb8043db912bb8ad6643130848184edad33358029a3176cabaec"
   ]
   
   static func successHttpURLResponse(request: URLRequest) -> URLResponse {
