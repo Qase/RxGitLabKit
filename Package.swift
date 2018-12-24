1e7cdc2b9ac6f6fc2418088d691afc9e37b2f913
Dagy Tran <dagytran@gmail.com>
2018-12-25 00:36:34 +0100

    SPM support. Readme update.

--- Package.swift
+++ Package.swift
@@ -18,6 +18,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "RxGitLabKit",
-            dependencies: ["RxSwift", "RxCocoa"])
+            dependencies: ["RxSwift", "RxCocoa"],
+            path: "Sources")
     ]
 )
