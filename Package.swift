1acc786f0389d9ad9501c4ea004c35efec5cff6b
Dagy Tran <dagytran@gmail.com>
2018-12-24 16:53:21 +0100

    Package

--- Package.swift
+++ Package.swift
@@ -1,31 +1,23 @@
-//
-//  Package.swift
-//  RxGitLabKit
-//
-//  Created by Dagy Tran on 03/08/2018.
-//
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 // The swift-tools-version declares the minimum version of Swift required to build this package.
+
 import PackageDescription
 
 let package = Package(
-  name: "RxGitLabKit",
-  products: [
-    // Products define the executables and libraries produced by a package, and make them visible to other packages.
-    .library(
-      name: "RxGitLabKit",
-      targets: ["RxGitLabKit"])
+    name: "RxGitLabKit",
+    products: [
+        .library(
+            name: "RxGitLabKit",
+            targets: ["RxGitLabKit"]),
     ],
-  targets: [
-    // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-    // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-    .target(
-      name: "RxGitLabKit",
-      dependencies: ["RxSwift"])
-  ],
     dependencies: [
-    // Dependencies declare other packages that this package depends on.
-    // .package(url: /* package url */, from: "1.0.0"),
-    .package(url: "https://github.com/ReactiveX/RxSwift.git", "4.0.0" ..< "5.0.0")
-  ],
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", "4.0.0" ..< "5.0.0")
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "RxGitLabKit",
+            dependencies: ["RxSwift", "RxCocoa"])
+    ]
 )
